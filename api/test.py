98dea437884e637795deb8f812119532f1bd3793
Jose508773 <59071207+Jose508773@users.noreply.github.com>
2026-01-31 16:46:35 -0800

    test preview

--- api/test.py
+++ api/test.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi import Body
 app = FastAPI()
 
 @app.get("/api/test")
 async def root():
     return {"response": "ITs working!!!!!!!"}
+
+
+people = {}
+@app.post("/api/test")
+async def add_person(body: dict = Body(...)):
+    users_name = body.get("userName", "")
+    users_age = body.get("userAge", 0)
+    people["name"] = users_name
+    people["age"] = users_age
+    return {"people": people}
--- timing.js
+++ timing.js
@@ -1,8 +1,27 @@
 let btn = document.getElementById("time-btn");
 let result = document.getElementById("time-result");
 
+let nameInput = document.getElementById("name-input");
+let ageInput = document.getElementById("age-input");
+let nameResult = document.getElementById("name-result");
+let ageResult = document.getElementById("age-result");
+let peopleResult = document.getElementById("people-result");
+let showPeopleBtn = document.getElementById("show-people-btn");
 
 
+
+async function add_person() {
+    let name = nameInput.value;
+    let age = ageInput.value;
+    let response = await fetch("api/test", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({userName: name, userAge: age})
+    })
+    
+    let data = await response.json();
+    peopleResult.textContent = data.people;
+}
 
 
 
